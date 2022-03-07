70da55270823f57d54e7979b5270be4b0ecaed3d
TheThomasY <twy1g16@soton.ac.uk>
2022-03-07 16:42:30 +0000

    styles: general structure laid out

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      Hello
+      <h1 className='question'>An animal cell contains:</h1>
+      <ul className="answers-list">
+        <li>Cell Wall Ribosomes</li>
+        <li>Cytoplasm Chloroplast</li>
+        <li>Partially permeable membrane Impermeable membrane</li>
+        <li>Cellulose Mitochondria</li>
+      </ul>
+      <h2 className='answers-status'>The answer is incorrect</h2>
     </div>
   );
 }
