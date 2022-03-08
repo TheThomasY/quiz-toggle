89b68843c483c248961811ceefbf1d259e7767e7
TheThomasY <twy1g16@soton.ac.uk>
2022-03-08 12:33:51 +0000

    feat: sass variables in js, selected bubble can be applied

--- src/components/AnswerOptions.tsx
+++ src/components/AnswerOptions.tsx
@@ -1,5 +1,9 @@
+// * React
+import React, { useState } from 'react';
+
 // * Styles
 import './AnswerOptions.scss';
+import colors from '../styles/_colors.module.scss';
 
 type Props = {
   answerOptions: string[]
@@ -7,11 +11,18 @@ type Props = {
 }
 
 export default function AnswerOptions({answerOptions, correct} : Props) {
+  const [selected, setSelected] = useState<number>(0);
+
+  let selectedBubble = {
+    borderRadius: '5rem',
+    backgroundColor: colors['orangeBorder'],
+    color: colors['orangeSelectedText']
+  };
 
   return (
     <li className='answer-options'>
       {answerOptions.map((option, index) => (
-        <div key={index + option}>{option}</div>
+        <div className={'single-option'} style={(index === selected) ? selectedBubble : undefined} key={index + option}>{option}</div>
       ))}
     </li>
   )
